58993679f23c815070d054138da823e6e3691389
abood1530 <abdalrahmanalkashef@gmail.com>
2026-05-23 23:19:34 +0300

    Add home screen

---
+++ lib/home_screen.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Text(
+                'Categories',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Spacer(),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  visualDensity: VisualDensity(
+                    vertical: VisualDensity.minimumDensity,
+                  ),
+                ),
+                child: Text(
+                  'see all',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        ConstrainedBox(
+          constraints: BoxConstraints(maxHeight: 100),
+          child: GridView.builder(
+            scrollDirection: Axis.horizontal,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              mainAxisSpacing: 5,
+            ),
+            padding: EdgeInsetsDirectional.symmetric(horizontal: 10),
+            itemBuilder: (context, index) {
+              return Container(color: Colors.blue.shade200, width: 100);
+            },
+            itemCount: 10,
+          ),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Text(
+                'Latest Product',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Spacer(),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  visualDensity: VisualDensity(
+                    vertical: VisualDensity.minimumDensity,
+                  ),
+                ),
+                child: Text(
+                  'see all',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+          ),
+          itemCount: 4,
+          physics: NeverScrollableScrollPhysics(),
+          padding: EdgeInsetsDirectional.symmetric(horizontal: 10),
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Container(color: Colors.blue.shade200, height: 200);
+          },
+        ),
+
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Text(
+                'Famous Product',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Spacer(),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  visualDensity: VisualDensity(
+                    vertical: VisualDensity.minimumDensity,
+                  ),
+                ),
+                child: Text(
+                  'see all',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          itemBuilder: (context, index) {
+            return Container(
+              color: Colors.blue.shade200,
+              height: 200,
+              margin: EdgeInsetsDirectional.only(bottom: 5),
+            );
+          },
+          shrinkWrap: true,
+          itemCount: 10,
+        ),
+
+        // SizedBox(
+        //   height: 100,
+        //   child: ListView.builder(
+        //     itemBuilder: (context, index) {
+        //       return Container(
+        //         color: Colors.green.shade200,
+        //         width: 100,
+        //         margin: EdgeInsetsDirectional.only(end: 5, start: 5),
+        //       );
+        //     },
+        //     itemCount: 20,
+        //     scrollDirection: Axis.horizontal,
+        //     // shrinkWrap: true,
+        //   ),
+        // ),
+      ],
+    );
+  }
+}
